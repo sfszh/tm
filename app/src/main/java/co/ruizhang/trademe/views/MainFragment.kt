723184e596fb8bb9ca332f919198f30ca18373f2
Rui Zhang <rayruizhang1@gmail.com>
2020-02-25 00:36:29 +1300

    add navigation controller

--- app/src/main/java/co/ruizhang/trademe/views/MainFragment.kt
+++ app/src/main/java/co/ruizhang/trademe/views/MainFragment.kt
@@ -1,7 +1,31 @@
 package co.ruizhang.trademe.views
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import co.ruizhang.trademe.databinding.FragmentMainBinding
 
-class MainFragment :Fragment() {
+class MainFragment : Fragment() {
+    private lateinit var binding: FragmentMainBinding
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.chooseButton.setOnClickListener {
+            val action = MainFragmentDirections.actionMainFragmentToSingleListingFragment("categoryid-placeholder")
+            findNavController().navigate(action)
+        }
+    }
 }
